b043f54a15c83c0b68016e2585ea763bce557e92
Luis E. Cerna <cernalue@gmail.com>
2018-05-21 06:51:38 -0500

    fixed issues

--- Flux/minimum_flow.py
+++ Flux/minimum_flow.py
@@ -59,30 +59,22 @@ for ascii_decimal in range(65, 69, 1): #65 is A, 90 is Z
     i += 1
 
 edges = { (n[0], n[1], 4), \
-          (n[0], n[2], 10), \
-          (n[1], n[3], 9), \
-          (n[2], n[3], 5), }
+          (n[0], n[2], 1), \
+          (n[1], n[3], 4), \
+          (n[2], n[3], 1), } 
 
-print(nodes_labels)
 F.add_nodes_from(nodes_labels)
 F.add_weighted_edges_from(edges)
 position = nx.spring_layout(F, k=k, iterations=20, scale=0.8) #CHANGE FOR OVERLAPS
 edges_labels = nx.get_edge_attributes(F,'weight')
 
+simplex_edge = edges
 
 status = nx.info(F) 
 print (status)
 nx.draw(F, position, with_labels=True)
+nx.draw_networkx_edges(F, position, edgelist=simplex_edge, edge_color='y', width=2)
 nx.draw_networkx_edge_labels(F, position, edge_labels=edges_labels)
-plt.axis('equal')
-plt.show()
-
-nx.draw(G, pos, with_labels=True)
-path = nx.shortest_path(G, source=n[0], target=n[7], weight='weight')
-path_edges = list(zip(path,path[1:]))
-nx.draw_networkx_nodes(G, pos, nodelist=path, node_color='y')
-nx.draw_networkx_edges(G,pos, edgelist=path_edges, edge_color='r', width=2)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edges_labels)
 plt.axis('equal')
 plt.show()
 
