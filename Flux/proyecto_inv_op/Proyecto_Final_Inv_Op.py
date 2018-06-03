1d0ed0d055ecf158877402dd6295d10f7856a7bc
Luis E. Cerna <cernalue@gmail.com>
2018-06-02 23:44:20 -0500

    Firts example completed. Need now to solve a real problem

--- Flux/proyecto_inv_op/Proyecto_Final_Inv_Op.py
+++ Flux/proyecto_inv_op/Proyecto_Final_Inv_Op.py
@@ -18,7 +18,7 @@ n_of_units = np.loadtxt(srcfile, float, skiprows=1, usecols=(2,3,4,5,6,7,8,9,10,
 size = np.size(n_of_units)
 
 weight = np.matmul(increment_per_unit, n_of_units)
-
+weigth = np.around(weight, decimals = 3)
 G = nx.DiGraph()
 
 nodes_labels = []
@@ -37,12 +37,14 @@ G.add_node('C', demand = 50)
 G.add_node('D', demand = 30)
 
 # NODE WEIGHT 
+edges = {}
 k = 0
-for i in range(4):
+for i in range(4): # mod if increment number of columns
     for j in range(4):
         if i != j:
-            G.add_edge(n[i], n[j], weight = weight[k])
-            k += k
+            G.add_edge(n[i], n[j], weight = np.around(weight[k], 3))
+            edges[k] = (n[i], n[j])
+            k += 1
 
 flowCost, flowDict = nx.network_simplex(G)
 print('flow cost = ', flowCost)
@@ -55,10 +57,31 @@ edges_labels = nx.get_edge_attributes(G,'weight')
 # PRINT PROBLEM
 
 nx.draw(G, position, with_labels=True)
-nx.draw_networkx_edges(G, position, edge_color='y', width=2)
-nx.draw_networkx_edge_labels(G, position, edge_labels=edges_labels)
+nx.draw_networkx_edges(G, position, edge_color='r', width=2)
+nx.draw_networkx_edge_labels(G, position, edge_labels=edges_labels, label_pos=0.3)
 plt.axis('equal')
 plt.show()
 
 # PRINT SOLUTION
+F = nx.DiGraph(flowDict)
 
+# READ DICTIONARY
+sol_edges = []
+i = -1
+for ascii_decimal in range(65, 69, 1): #65 is A, 90 is Z
+    node1 = chr(ascii_decimal)
+    j = -1
+    i += 1
+    for node2 in flowDict[node1]:
+        j += 1
+        if i == j:
+            j += 1
+        F.add_edge(n[i], n[j], weight = np.around(flowDict[node1][node2], 3))
+
+sol_edges_labels = nx.get_edge_attributes(F,'weight')
+
+nx.draw(F, position, with_labels=True)
+nx.draw_networkx_edges(F, position, edge_color='y', width=2)
+nx.draw_networkx_edge_labels(F, position, edge_labels=sol_edges_labels, label_pos=0.3)
+plt.axis('equal')
+plt.show()
